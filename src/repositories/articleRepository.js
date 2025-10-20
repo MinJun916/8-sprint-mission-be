3371a37dfe41f0f34c36709ddd4fe35427268eea
MinJun916 <ed125248@gmail.com>
2025-10-20 17:45:31 +0900

    fix: resolve Prisma errors in ArticleRepository.js

--- src/repositories/articleRepository.js
+++ src/repositories/articleRepository.js
@@ -3,14 +3,14 @@ import prisma from '../middlewares/prisma.js';
 export const createArticle = async (title, content) => {
   return await prisma.article.create({
     data: { title, content },
-    select: { id: true, title: true, content: true, like: true, createdAt: true },
+    select: { id: true, title: true, content: true, likeCount: true, createdAt: true },
   });
 };
 
 export const getArticles = async (whereCondition, orderBy, skip, take) => {
   return await prisma.article.findMany({
     where: whereCondition,
-    select: { id: true, title: true, content: true, like: true, createdAt: true },
+    select: { id: true, title: true, content: true, likeCount: true, createdAt: true },
     skip,
     take,
     orderBy,
@@ -26,7 +26,7 @@ export const getArticleCount = async (whereCondition) => {
 export const getArticleById = async (id) => {
   return await prisma.article.findUnique({
     where: { id },
-    select: { id: true, title: true, content: true, like: true, createdAt: true },
+    select: { id: true, title: true, content: true, likeCount: true, createdAt: true },
   });
 };
 
@@ -40,7 +40,7 @@ export const updateArticle = async (id, title, content) => {
   return await prisma.article.update({
     where: { id },
     data: { title, content },
-    select: { id: true, title: true, content: true, like: true, createdAt: true },
+    select: { id: true, title: true, content: true, likeCount: true, createdAt: true },
   });
 };
 
